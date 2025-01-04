1c1eb45c904b0f1f02671d33ce3ee603b674c4e6
kzm0705 <tyoityotyoi@gmail.com>
2025-01-04 15:27:19 +0900

    tukareta

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import PySimpleGUI as sg
-
+from PIL import Image
 
 def main():
-    layout = [[sg.Text('hello world')],
-              [sg.Button('OK')]]
+    defult_image = Image.open('.\imagesPng\Pokémon logo.png')
+    #画像のサイズを変更
+    defult_image.thumbnail((400,400))
+    #変更した画像を一時ファイルに保持
+    defult_image.save('.\\imagesPng\\resized_image.png')
+
+    layout = [[sg.Image(filename='.\\imagesPng\\resized_image.png', key='-image-')],
+              [sg.Button('OK',expand_x=True,expand_y=True),sg.Button('イーブイシリーズ',expand_x=True, expand_y=True)]]
     
-    window = sg.Window('Sample window', layout)
+    window = sg.Window('ポケモン図鑑', layout, resizable=True)
 
     while True:
         event, values = window.read()
---
+++ weapon.py
@@ -0,0 +1,3 @@
+from PIL import Image, ImageFilter
+
+image = Image.open('C:\\Users\\pumpk\\Desktop\\ImageDisplay\\imagesPng\\be845fb5-384a-41ed-b0e6-c49e04daec5e.png')
